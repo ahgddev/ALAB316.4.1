63c7d3f69e308630c2da159dbc694739345363ea
ahgddev <ahgddev@gmail.com>
2024-01-30 18:03:21 -0700

    Fixed password input coming in blank. Just need to do password checking.

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
 let regForm = document.getElementById("registration")
-let pswd = document.getElementsByName("password")[0].value
-let checkPswd = document.getElementsByName("passwordCheck")[0].value
 
 document.getElementsByName("password")[0].addEventListener("change", function(){
-    validateInitialPassword()
+    let pswd = document.getElementsByName("password")[0].value
+    validateInitialPassword(pswd)
 });
 
 document.getElementsByName("passwordCheck")[0].addEventListener("change", function(){
-    validatePswdCheck()
+    let pswd = document.getElementsByName("password")[0].value
+    let checkPswd = document.getElementsByName("passwordCheck")[0].value
+    validatePswdCheck(pswd, checkPswd)
 })
 
-pswdCheck.addEventListener("change", validatePswdCheck());
-
-
-function validateInitialPassword(){
-   if(str){
-    
+function validateInitialPassword(str){
+   if(str == "123456789201"){
+        console.log("Wrong!")
    }
 }
 
-function validatePswdCheck(){
-    if(pswd != checkPswd){
+function validatePswdCheck(pstr, pstrcheck){
+    if(pstr != pstrcheck){
         return false
     } else {
         return true
